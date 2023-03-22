4d6f7166b535466faa8d78d0c552dcf05e41377c
Elwahbi1988 <amineelwahbi@outlook.fr>
2023-03-22 15:50:23 +0000

    positive_or_negative.c

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -5,7 +5,7 @@
 *@i: determinet i is positve or negative or zero.
 *Return: 0
 */
-void positive_or_negative(int i);
+void positive_or_negative(int i)
 {
 if (i > 0)
 printf("%d is possitive\n", i);
