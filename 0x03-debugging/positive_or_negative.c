06873700cde49f6a11e7d253a32ce7d858417fa6
Elwahbi1988 <amineelwahbi@outlook.fr>
2023-03-22 15:42:35 +0000

    positive_or_negative.c

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+*positive_or_negative function - Test function for positive or negative.
+*Description: void positive_or_negative function for i.
+*@i: determinet i is positve or negative or zero.
+*Return: 0
+*/
+void positive_or_negative(int i);
+{
+if (i > 0)
+printf("%d is possitive\n", i);
+else if (i < 0)
+printf("%d is negative\n", i);
+else
+printf("%d is zero\n", i);
+}
