159f768a7158cbb29e6bb33ca6f222beeb8d6e0b
Elwahbi1988 <amineelwahbi@outlook.fr>
2023-04-05 21:31:03 +0000

    2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -4,13 +4,13 @@
   * @s: the string to be measured
   * Return: the length of the string
   */
-int_strlen_recursion(char *s)
+int _strlen_recursion(char *s)
 {
 int longit = 0;
 if (*s)
 {
 longit++;
-longit == _strlen_recursion(s + 1);
+longit += _strlen_recursion(s + 1);
 }
 return (longit);
 }
