0ce65d5e863d0e04168acf94e80ebdb74e10b6dc
Elwahbi1988 <amineelwahbi@outlook.fr>
2023-04-20 00:28:02 +0000

    3-get_op_func.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,4 +1,6 @@
 #include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
 /**
 *get_op_func - selects the correct function
 *to perform the operation asked by user
@@ -9,13 +11,14 @@
 int (*get_op_func(char *s))(int, int)
 {
 op_t ops[] = {
-{"+", op_add},
-{"-", op_sub},
-{"*", op_mul},
-{"/", op_div},
-{"%", op_mod},
-{NULL, NULL}
+	{"+", op_add},
+	{"-", op_sub},
+	{"*", op_mul},
+	{"/", op_div},
+	{"%", op_mod},
+	{NULL, NULL}
 };
+
 int i = 0;
 while (ops[i].op != NULL && *(ops[i].op) != *s)
 i++;
