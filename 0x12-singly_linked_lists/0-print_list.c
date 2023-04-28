bda4f9e18f8f974c1722d5e618425f3b497d629a
Elwahbi1988 <amineelwahbi@outlook.fr>
2023-04-28 00:22:28 +0000

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "lists.h"
+
 /**
 *print_list - Prints all elements of liked list
 *@h: Pointer to the list to print
@@ -8,9 +9,12 @@
 size_t print_list(const list_t *h)
 {
 size_t s = 0;
+
 while (h)
 {
 if (!h->str)
+printf("[0] (nil)\n");
+else
 printf("[%u] %s\n", h->len, h->str);
 h = h->next;
 s++;
