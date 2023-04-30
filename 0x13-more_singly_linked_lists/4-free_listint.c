432dcee81820c8429ef9228d55ce9128ce1de3f2
Elwahbi1988 <amineelwahbi@outlook.fr>
2023-04-30 11:20:30 +0000

    4-free_listint.c

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+/**
+*free_listint - Free a linked list
+*@head: List listint_t to be freed
+*/
+void free_listint(listint_t *head)
+{
+listint_t *temp;
+while (head)
+{
+temp = head->next;
+free(head);
+head = temp;
+}
+}
